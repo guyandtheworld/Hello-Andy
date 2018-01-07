1d3dda13c35b21d330a459770bb6c826eafdb034
Adarsh S <4darshofficial@gmail.com>
2018-01-07 16:03:47 +0530

    App with Database  connection

---
+++ SQL/app/src/main/java/com/example/rustbucket/sql/DbStrings.java
@@ -0,0 +1,13 @@
+package com.example.rustbucket.sql;
+
+/**
+ * Created by RustBucket on 07-01-2018.
+ */
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "127.0.0.1:3306";
+    static final String DATABASE_NAME = "mhs_example";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "password";
+}
---
+++ SQL/app/src/main/java/com/example/rustbucket/sql/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.rustbucket.sql;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    ItemAdapter itemAdapter;
+    Context thisContext;
+    ListView myListView;
+    TextView progressTextView;
+    Map<String, Double> fruitsMap = new LinkedHashMap<String, Double>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        progressTextView = (TextView) findViewById(R.id.progressTextView);
+
+        thisContext = this;
+        progressTextView.setText("");
+        Button getDataButton = (Button) findViewById(R.id.getDataButton);
+        getDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetData retrieveData = new GetData();
+                retrieveData.execute("");
+            }
+        });
+    }
+
+    private class GetData extends AsyncTask<String, String, String>{
+
+        String msg = "";
+
+        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        static final String DB_URL = "jdbc:mysql://" +
+                DbStrings.DATABASE_URL + "/" +
+                DbStrings.DATABASE_NAME;
+
+        @override
+        protected void  onPreExecute(){
+            progressTextView.setText("Connecting to database...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+}
